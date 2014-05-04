73b51b0051fde39af27dc67a5d93162edfc21b2b
boB Rudis <bob@rudis.net>
2014-05-04 09:00:06 -0400

    Updated example code & graphics

--- R/Rforecastio.R
+++ R/Rforecastio.R
@@ -13,21 +13,38 @@
 
 require(RJSONIO)
 require(RCurl)
+require(plyr)
 
 #' obtain data from http://forecast.io
 #'
 #' @param api.key your forecast.io API key
 #' @param latitude forecast latitude (character, decimal format)
 #' @param longitude forecast longitude (character, decimal format)
-#' @param for.time unix timestamp if requesting history
+#' @param for.time unix timestamp if requesting history (optional)
 #' @export
 #' @return a list of three named elements:\cr
 #' \itemize{
 #' \item \code{json} which is the converted (to R object) JSON from forecast.io, which is great for extracting the metadata
 #' \item \code{hourly.df} which has the hourly readings/forecast
+#' \item \code{daily.df} which has the daily summary/forecast
 #' \item \code{minutely.df} which is NULL if "for.time" was not used, otherwise will have the "minutely" forecast as a data frame
 #' }
-
+#' @examples
+#' \dontrun{
+#' # NEVER put API keys in revision control systems or source code!
+#' fio.api.key = readLines("~/.forecast.io")
+#'
+#' my.latitude = "43.2673"
+#' my.longitude = "-70.8618"
+#'
+#' # get data for "no"
+#' fio.list <- fio.forecast(fio.api.key, my.latitude, my.longitude)
+#'
+#' # or
+#'
+#' fio.list <- fio.forecast(fio.api.key, my.latitude, my.longitude, for.time=as.integer(Sys.time())-(60*60*6))
+#' }
+#'
 fio.forecast <- function(api.key, latitude, longitude, for.time) {
 
   # using RCurl's getURLContent() since it fully supports http or https
@@ -43,35 +60,32 @@ fio.forecast <- function(api.key, latitude, longitude, for.time) {
   fio <- fromJSON(fio.json)
 
   # extract hourly forecast data
-  fio.hourly.df <- data.frame(
-    time = as.POSIXlt(sapply(fio$hourly$data,"[[","time"), origin="1970-01-01"),
-    summary = sapply(fio$hourly$data,"[[","summary"),
-    icon = sapply(fio$hourly$data,"[[","icon"),
-    precipIntensity = sapply(fio$hourly$data,"[[","precipIntensity"),
-    temperature = sapply(fio$hourly$data,"[[","temperature"),
-    apparentTemperature = sapply(fio$hourly$data,"[[","apparentTemperature"),
-    dewPoint = sapply(fio$hourly$data,"[[","dewPoint"),
-    windSpeed = sapply(fio$hourly$data,"[[","windSpeed"),
-    windBearing = sapply(fio$hourly$data,"[[","windBearing"),
-    cloudCover = sapply(fio$hourly$data,"[[","cloudCover"),
-    humidity = sapply(fio$hourly$data,"[[","humidity"),
-    pressure = sapply(fio$hourly$data,"[[","pressure"),
-    visibility = sapply(fio$hourly$data,"[[","visibility"),
-    ozone = sapply(fio$hourly$data,"[[","ozone")
-  )
+  fio.hourly.df <- ldply(fio$hourly$data, data.frame)
+  fio.hourly.df$time <- as.POSIXlt(fio.hourly.df$time, origin="1970-01-01")
+
+  # extract daily forecast data
+  fio.daily.df <-  ldply(fio$daily$data, data.frame)
+  fio.daily.df$time <- as.POSIXlt(fio.daily.df$time, origin="1970-01-01")
+  fio.daily.df$sunsetTime <- as.POSIXlt(fio.daily.df$sunsetTime, origin="1970-01-01")
+  fio.daily.df$sunsetTime <- as.POSIXlt(fio.daily.df$sunsetTime, origin="1970-01-01")
+  fio.daily.df$temperatureMinTime <- as.POSIXlt(fio.daily.df$temperatureMinTime, origin="1970-01-01")
+  fio.daily.df$temperatureMaxTime <- as.POSIXlt(fio.daily.df$temperatureMaxTime, origin="1970-01-01")
+  fio.daily.df$apparentTemperatureMinTime <- as.POSIXlt(fio.daily.df$apparentTemperatureMinTime, origin="1970-01-01")
+  fio.daily.df$apparentTemperatureMaxTime <- as.POSIXlt(fio.daily.df$apparentTemperatureMaxTime, origin="1970-01-01")
+  fio.daily.df$precipIntensityMaxTime <- as.POSIXlt(fio.daily.df$precipIntensityMaxTime, origin="1970-01-01")
 
   # 'minutely' only returns for current forecast
   if (missing(for.time)) {
     # extract minutely forecast data
-    fio.minutely.df <- data.frame(
-      time = as.POSIXlt(sapply(fio$minutely$data,"[[","time"), origin="1970-01-01"),
-      precipIntensity = sapply(fio$minutely$data,"[[","precipIntensity"),
-      precipProbability = sapply(fio$minutely$data,"[[","precipProbability")
-    )
+    fio.minutely.df <- ldply(fio$minutely$data)
+    fio.minutely.df$time <- as.POSIXlt(fio.minutely.df$time, origin="1970-01-01")
   } else {
     fio.minutely.df <- NULL
   }
 
-  return(list(json=fio, hourly.df=fio.hourly.df, minutely.df=fio.minutely.df))
+  return(list(json=fio,
+              hourly.df=fio.hourly.df,
+              daily.df=fio.daily.df,
+              minutely.df=fio.minutely.df))
 
 }
