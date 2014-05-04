179f9ec3c1f07278e277844c2900840a70338d0d
boB Rudis <bob@rudis.net>
2014-05-04 08:01:41 -0400

    RCurl call fix; documentation

--- R/Rforecastio.R
+++ R/Rforecastio.R
@@ -1,12 +1,12 @@
-# 
+#
 # Rforecastio
 #
 # Simple R interface to forecast.io JSON weather data
-# 
+#
 # API Docs: https://developer.forecast.io/docs/v2
 # Dev site: https://developer.forecast.io/
 #
-# Copyright (c) 2013 Bob Rudis (@hrbrmstr) bob@rudis.net
+# Copyright (c) 2014 Bob Rudis (@hrbrmstr) bob@rudis.net
 #
 # MIT License
 #
@@ -14,45 +14,35 @@
 require(RJSONIO)
 require(RCurl)
 
-#
-# fio.forecast()
-#
-# obtain data from http://forecast.io
-#
-# for just the forecat, pass in your:
-#
-# - forecast.io api key (character)
-# - latitude (characeter)
-# - longitude (character)
-# 
-# for history, pass the time (unix timestamp format) to for.time
-#
-# returns a list of three named elements:
-#
-# - "json" which is the converted (to R object) JSON from forecast.io,
-#   which is great for extracting the metadata
-#
-# - "hourly.df" which has the hourly readings/forecast
-#
-# - "minutely.df" which is NULL if "for.time" was not used, otherwise will have
-#   the "minutely" forecast as a data frame
-#
+#' obtain data from http://forecast.io
+#'
+#' @param api.key your forecast.io API key
+#' @param latitude forecast latitude (character, decimal format)
+#' @param longitude forecast longitude (character, decimal format)
+#' @param for.time unix timestamp if requesting history
+#' @export
+#' @return a list of three named elements:\cr
+#' \itemize{
+#' \item \code{json} which is the converted (to R object) JSON from forecast.io, which is great for extracting the metadata
+#' \item \code{hourly.df} which has the hourly readings/forecast
+#' \item \code{minutely.df} which is NULL if "for.time" was not used, otherwise will have the "minutely" forecast as a data frame
+#' }
 
 fio.forecast <- function(api.key, latitude, longitude, for.time) {
-  
+
   # using RCurl's getURLContent() since it fully supports http or https
   if (missing(for.time)) {
     fio.json <- getURLContent(sprintf("https://api.forecast.io/forecast/%s/%s,%s",
-                                      api.key, latitude, longitude), ssl.verifypeer = FALSE)
+                                      api.key, latitude, longitude))
   } else {
     fio.json <- getURLContent(sprintf("https://api.forecast.io/forecast/%s/%s,%s,%d",
-                                      api.key, latitude, longitude, for.time), ssl.verifypeer = FALSE)
+                                      api.key, latitude, longitude, for.time))
   }
-  
+
   # take the JSON blob we got from forecast.io and make an R list from it
   fio <- fromJSON(fio.json)
-  
-  # extract hourly forecast data  
+
+  # extract hourly forecast data
   fio.hourly.df <- data.frame(
     time = as.POSIXlt(sapply(fio$hourly$data,"[[","time"), origin="1970-01-01"),
     summary = sapply(fio$hourly$data,"[[","summary"),
@@ -69,10 +59,10 @@ fio.forecast <- function(api.key, latitude, longitude, for.time) {
     visibility = sapply(fio$hourly$data,"[[","visibility"),
     ozone = sapply(fio$hourly$data,"[[","ozone")
   )
-  
+
   # 'minutely' only returns for current forecast
   if (missing(for.time)) {
-    # extract minutely forecast data  
+    # extract minutely forecast data
     fio.minutely.df <- data.frame(
       time = as.POSIXlt(sapply(fio$minutely$data,"[[","time"), origin="1970-01-01"),
       precipIntensity = sapply(fio$minutely$data,"[[","precipIntensity"),
@@ -81,7 +71,7 @@ fio.forecast <- function(api.key, latitude, longitude, for.time) {
   } else {
     fio.minutely.df <- NULL
   }
-  
+
   return(list(json=fio, hourly.df=fio.hourly.df, minutely.df=fio.minutely.df))
-  
+
 }
